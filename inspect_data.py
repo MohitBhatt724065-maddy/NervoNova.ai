246fc267ea62aa8031f5b0e58a42ed11b7e39312
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2026-05-08 20:42:07 +0545

    Day 3 recap and what actually i did.

---
+++ inspect_data.py
@@ -0,0 +1,22 @@
+# What i exactlt did i save in Day 3?
+import numpy as np
+import os
+
+train_shape = np.load("Processed_Data/train_shape.npy")
+test_shape = np.load("Processed_Data/test_shape.npy")
+
+print("Training Data!")
+print(f"Total images : {train_shape[0]}")
+print(f"Image Size : {train_shape[1]} x {train_shape[2]}")
+print(f"Color Channels : {train_shape[3]}")
+
+print("Testing Data!")
+print(f"Total images : {test_shape[0]}")
+print(f"Image Size : {test_shape[1]} x {test_shape[2]}")
+print(f"Color Channels : {test_shape[[3]]}" )
+
+
+for f in ["test_X.dat", "test_y.dat", "train_X.dat", "train_y.dat"]:
+    path = f"Processed_Data/{f}"
+    size = os.path.getsize(path)/(1024**3)
+    print(f"{f:20s} --> {size:.2f} GB")
